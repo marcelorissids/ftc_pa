f06a3ef8ecbcd37641a08e93a496e1c8cbbe63dd
Marcelo Rissi <marcelorissi2@gmail.com>
2023-04-13 23:17:05 -0300

    Add pages

---
+++ pages/Cities.py
@@ -0,0 +1,49 @@
+import streamlit as st
+
+import utils.cities_data as cdt
+
+
+def make_sidebar(df):
+    st.sidebar.markdown("## Filtros")
+
+    countries = st.sidebar.multiselect(
+        "Escolha os Paises que Deseja visualizar as Informações",
+        df.loc[:, "country"].unique().tolist(),
+        default=["Brazil", "England", "Qatar", "South Africa", "Canada", "Australia"],
+    )
+
+    return list(countries)
+
+
+def main():
+    st.set_page_config(page_title="Cities", page_icon="🏙️", layout="wide")
+
+    df = cdt.read_processed_data()
+
+    countries = make_sidebar(df)
+
+    st.markdown("# :cityscape: Visão Cidades")
+
+    fig = cdt.top_cities_restaurants(countries)
+
+    st.plotly_chart(fig, use_container_width=True)
+
+    best, worst = st.columns(2)
+
+    with best:
+        fig = cdt.top_best_restaurants(countries)
+
+        st.plotly_chart(fig, use_container_width=True)
+
+    with worst:
+        fig = cdt.top_worst_restaurants(countries)
+
+        st.plotly_chart(fig, use_container_width=True)
+
+    fig = cdt.most_cuisines(countries)
+
+    st.plotly_chart(fig, use_container_width=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ pages/Countries.py
@@ -0,0 +1,51 @@
+import streamlit as st
+
+import utils.countries_data as cdt
+
+
+def make_sidebar(df):
+    st.sidebar.markdown("## Filtros")
+
+    countries = st.sidebar.multiselect(
+        "Escolha os Paises que Deseja visualizar as Informações",
+        df.loc[:, "country"].unique().tolist(),
+        default=["Brazil", "England", "Qatar", "South Africa", "Canada", "Australia"],
+    )
+
+    return list(countries)
+
+
+def main():
+    st.set_page_config(page_title="Countries", page_icon="🌍", layout="wide")
+
+    df = cdt.read_processed_data()
+
+    countries = make_sidebar(df)
+
+    st.markdown("# :earth_americas: Visão Países")
+
+    fig = cdt.countries_restaurants(countries)
+
+    st.plotly_chart(fig, use_container_width=True)
+
+    fig = cdt.countries_cities(countries)
+
+    st.plotly_chart(fig, use_container_width=True)
+
+    votes, plate_price = st.columns(2)
+
+    with votes:
+        fig = cdt.countries_mean_votes(countries)
+
+        st.plotly_chart(fig, use_container_width=True)
+
+    with plate_price:
+        fig = cdt.countries_average_plate(countries)
+
+        st.plotly_chart(fig, use_container_width=True)
+
+    return None
+
+
+if __name__ == "__main__":
+    main()
